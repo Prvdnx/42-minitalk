ce3670ceb27fabd5a0ca6327edcf044c4575a730
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2023-09-13 07:15:04 +0700

    all

--- client.c
+++ client.c
@@ -6,19 +6,30 @@
 /*   By: ookamonu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 01:24:19 by ookamonu          #+#    #+#             */
-/*   Updated: 2023/09/12 06:59:53 by ookamonu         ###   ########.fr       */
+/*   Updated: 2023/09/13 07:09:28 by ookamonu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+/*----------------------Message-Receipt-Acknowledgment-----------------------*\
+end function handles SIGUSR2 signals, indicating successful message transmission.
+\*---------------------------------------------------------------------------*/
 void	end(int sig)
 {
 	if (sig == SIGUSR2)
-		ft_putstr("\nMESSAGE RECIEVED CORRECTLY!\n\n", STDOUT_FILENO);
+		ft_putstr("\nMESSAGE SENT & RECIEVED CORRECTLY!\n\n", STDOUT_FILENO);
 	return ;
 }
 
+/*--------------------------[ 3 ]-Sending-Characters-------------------------*\
+send_char function is responsible for sending each character as a series of bits
+(0s & 1s) to server using signals (SIGUSR1 and SIGUSR2). It iterates through each
+of the character's 8 bits. For each bit, it checks if it's 1 or a 0 using bitwise
+operations (c & (0b10000000 >> bit)). If it's a 1, it sends SIGUSR1; if it's a 0,
+it sends SIGUSR2. After sending all 8 bits, it adds a small delay (usleep(300);)
+before sending the next character.
+\*---------------------------------------------------------------------------*/
 void	send_char(char c, int server_pid)
 {
 	int	bit;
@@ -31,10 +42,16 @@ void	send_char(char c, int server_pid)
 		else
 			kill(server_pid, SIGUSR2);
 		++bit;
-		usleep(300);
+		usleep(500);
 	}
 }
 
+/*------------------------[ 2 ]-Client-Initialization------------------------*\
+Client takes 2 command-line args, Server's PID & Message to be sent. It also
+registers "end" function 2 handle SIGUSR2 signals, indicating successful message
+transmission. Client enters a loop to iterate through each character in message.
+For each character, it calls the send_char function to send it to the server.
+\*---------------------------------------------------------------------------*/
 int	main(int ac, char **av)
 {
 	int	i;
@@ -52,3 +69,8 @@ int	main(int ac, char **av)
 		send_char(av[2][i++], server_pid);
 	send_char('\0', server_pid);
 }
+/*---------------------------[ 5 ]-End-Communication-------------------------*\
+After sending all characters of the message, the Client sends a null character
+('\0') to the server using the send_char function to signals to the server that
+the message is complete. The Client then terminates.
+\*---------------------------------------------------------------------------*/
--- server.c
+++ server.c
@@ -6,12 +6,26 @@
 /*   By: ookamonu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 23:15:25 by ookamonu          #+#    #+#             */
-/*   Updated: 2023/09/12 07:00:23 by ookamonu         ###   ########.fr       */
+/*   Updated: 2023/09/13 07:13:27 by ookamonu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+/*------------------[ 4 ]-Receiving-&-Processing-Characters------------------*\
+Server is still waiting in the while (1) loop to receive signals from the client.
+When the server receives a SIGUSR1 signal, it interprets it as 1 bit & sets the
+corresponding bit in the character being built (c).	Then increments the bit
+counter to move to the next bit. This process continues until all 8 bits of the
+character are received.
+Once a complete character is received, the server checks if it's a null character
+('\0'), which marks the end of the message. If it's a null character, the server
+prints a newline character to the standard output & sends *SIGUSR2* signal back
+to the client to indicate successful message reception. If not a null character,
+the server simply writes the received character to the standard output.
+After processing a character, the server resets the character and bit counters
+for the next character.
+\*---------------------------------------------------------------------------*/
 void	bit_handler(int sig, siginfo_t *info, void *more_info)
 {
 	static char	c = 0b00000000;
@@ -35,6 +49,13 @@ void	bit_handler(int sig, siginfo_t *info, void *more_info)
 	}
 }
 
+/*------------------------[ 1 ]-Server Initialization------------------------*\
+The server initializes, it prints a welcome message & its Process ID (PID)
+to standard output. It sets up signal handling using sigaction for both SIGUSR1 &
+SIGUSR2. The server enters an infinite loop "while (1) pause();" wherenit waits
+for incoming signals. This loop runs indefinitely, ready to receive messages from
+the client.
+\*---------------------------------------------------------------------------*/
 int	main(int ac, char **av)
 {
 	struct sigaction	sa;
@@ -62,3 +83,15 @@ int	main(int ac, char **av)
 	while (1)
 		pause();
 }
+
+/*
+struct sigaction
+{
+	void (*sa_handler)(int);  =Pointer to signal handling function (deprecated).
+	void (*sa_sigaction)(int, siginfo_t *, void *); =signal handl func (modern).
+	sigset_t sa_mask;  =Set of signals to be blocked during execution of handler.
+	int sa_flags;         =Special flags controlling the behavior of the signal.
+	void (*sa_restorer)(void);   =Not used in modern implementations.
+};
+Tester: https://github.com/ThibaudM13/minitalk-Tester/blob/main/tester.sh
+*/
